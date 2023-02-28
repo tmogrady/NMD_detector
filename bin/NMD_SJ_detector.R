b1c945ba2ac15042be399fb224fe0800c8f6f83d
tmogrady <cogrady@tulane.edu>
2023-02-28 10:05:30 -0600

    Make SJ data into GRanges object

--- bin/NMD_SJ_detector.R
+++ bin/NMD_SJ_detector.R
@@ -6,13 +6,22 @@ library("GenomicRanges")
 #read in STAR SJ.out.tab file
 sj <- read.table("input_data/MC1_truncated_test-SJ.out.tab")
 
-#name columns for my own sanity
-colnames(sj) <- c("chr","int_start","int_end","strand","motif","ann","unique","multi","overhang")
+#name columns for GRanges (and my own sanity)
+colnames(sj) <- c("chr","start","end","strand","motif","ann","unique","multi","overhang")
+#change strand info to +/- for GRanges
+sj <- sj %>%
+  mutate(strand = ifelse(strand == 1, "+",
+                         ifelse(strand == 2, "-",
+                                strand)))
 
 #first, get annotated genes (transcripts?) for each SJ
+sj_gr <- makeGRangesFromDataFrame(sj, keep.extra.columns = TRUE)
+
+
 #check if coding
 #check if SJ coordinates here are annotated (check donor & acceptor separately)
-#if so, look at exons between and divide by 3 to check for frameshift
+#if so, check if there are annotated exons between
+#if so, look at exon(s) and divide by 3 to check for frameshift
     #how to deal with multiple isoforms?
 #if no frameshift, assume no NMD
 #if frameshift, check for PTC
