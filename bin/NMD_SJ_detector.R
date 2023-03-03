38fa0be387ff8c118e5492382d1eccbcad6f0f9a
tmogrady <cogrady@tulane.edu>
2023-03-03 10:31:13 -0600

    check all transcripts of a single gene for skipped exons

--- bin/NMD_SJ_detector.R
+++ bin/NMD_SJ_detector.R
@@ -40,11 +40,78 @@ sj_pc <- sj_ann[sj_ann$gene_biotype == "protein_coding"]
 #may need to subset ann_gtf by gene's location, 
 #or otherwise work around metadata NA errors
 #this is the first (annotated, protein-coding) example SJ's gene:
-gene_test_gr <- ann_gtf[ann_gtf$gene_id == "ENSG00000122435"]
-gene_test_df <- data.frame(gene_test_gr)
+gene_gr <- ann_gtf[ann_gtf$gene_id == "ENSG00000122435"]
+#move to dataframes because they're easier and avoid GRanges metadata NA problem
+gene_df <- data.frame(gene_gr)
+transcripts <- unique(gene_df$transcript_id)
+transcript_df <- gene_df %>%
+  filter(transcript_id == "ENST00000370141") %>%
+  filter(type == "CDS")
+
+for (transcript in transcripts) {
+  if (is.na(transcript)) {
+    next
+  }
+  else {
+    print(transcript)
+    transcript_df <- gene_df %>%
+      filter(transcript_id == transcript) %>%
+      filter(type == "CDS")
+    if (nrow(transcript_df) == 0) {
+      print(paste(transcript, "has no CDS"))
+      next
+    }
+    else {
+      relevant <- data.frame()
+      for (i in 1:nrow(transcript_df)) {
+        if (nrow(relevant) == 0) {
+          if (start(sj_pc[1])-1 == transcript_df[i,3]) {
+            relevant <- transcript_df[i,]
+          }
+          else {
+            next
+          }
+        }
+        else { #if there is content in "relevant"
+          #print(i)
+          if (end(sj_pc[1])+1 == transcript_df[i,2]) {
+            relevant <- rbind(relevant, transcript_df[i,])
+            break
+          }
+          else {
+            relevant <- rbind(relevant, transcript_df[i,])
+          }
+        }
+      }
+    }
+
+    if (nrow(relevant) == 2) {
+      print("no SE")
+    }
+    else {
+      print("SE!")
+    }
+  }
+}
+
+
 ann_gtf[ann_gtf$transcript_id == "ENST00000370143"]
 #Error: logical subscript contains NAs
 #could possibly get around this error by subsetting based on genomic coordinates of gene
+
+#get (only) the CDS exons
+#if it matches the end of one, check the beginning of the next
+#if it matches, exit: it's an annotated splice junction that doesn't skip an exon in that isoform
+#if it doesn't match, store that exon and check the next one
+#continue till it matches
+#then take the skipped exon(s) and divide by 3
+#if divisible, it's an in-frame skip
+
+#to assess things at the end, will need to somehow compare depth (not just number I think)
+#of NMD-targeting splice junctions to other splice junctions
+#becuase eveerything is going to have some of both
+
+#at end of transcripts for that gene, put SJ into NMD list or not-NMD list
 
 
 #previous attempts with bed file:
