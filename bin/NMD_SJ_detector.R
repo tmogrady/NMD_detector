8b04f24ddc77452c9803dbbc35f0b668b2c09060
tmogrady <cogrady@tulane.edu>
2023-03-10 15:47:03 -0600

    Cleaned up commenting, removed some vestigial stuff

--- bin/NMD_SJ_detector.R
+++ bin/NMD_SJ_detector.R
@@ -5,7 +5,6 @@ library("GenomicRanges")
 library("GenomicFeatures")
 library("rtracklayer")
 library("BSgenome.Hsapiens.UCSC.hg38")
-
 #library("ensembldb")
 #library("EnsDb.Hsapiens.v86") #most recent version available
 
@@ -40,12 +39,10 @@ sj_pc <- sj_ann[sj_ann$gene_biotype == "protein_coding"]
 
 #next, for each SJ & gene combo,
 #run through the possible transcripts and check for NMD triggering
-#may need to subset ann_gtf by gene's location, 
-#or otherwise work around metadata NA errors
 #this is the first (annotated, protein-coding) example SJ's gene:
 gene_gr <- ann_gtf[ann_gtf$gene_id == "ENSG00000122435"]
 #get only CDS. That's what we need
-#and this will avoid errors later when subsetting on transcript
+#and this will avoid NA errors later when subsetting on transcript
 gene_cds_gr <- gene_gr[gene_gr$type == "CDS"]
 #move to dataframes because they're easier and avoid GRanges metadata NA problem
 gene_df <- data.frame(gene_cds_gr)
@@ -79,7 +76,6 @@ for (transcript in transcripts) { #go through each transcript
           }
         }
         else { #if there is content in "relevant" (i.e. we've found a matching donor)
-          #print(i)
           if (end(sj_pc[1])+1 == transcript_df[i,2]) { #look for matching acceptor
             relevant <- rbind(relevant, transcript_df[i,])
             break #if matching acceptor is found, we have all the exons we need
@@ -91,16 +87,16 @@ for (transcript in transcripts) { #go through each transcript
       }
     } #end of transcript
     if (nrow(relevant) == 0 | (end(sj_pc[1])+1 != relevant[nrow(relevant),2])) {
-      print("splice sites not annotated in this transcript")
+      print("splice sites not annotated in this transcript") #ignore for now
     }  
     else {
       if (nrow(relevant) == 2) {
-        print("annotated SJ: no new SE")
+        print("annotated SJ: no new SE") #assume no NMD
       }
       else {
         se <- relevant[2:(nrow(relevant) - 1), ]
         se$exon_length <- se$end - se$start + 1 #could use width column here
-        if (sum(se$exon_length) %% 3 == 0) {
+        if (sum(se$exon_length) %% 3 == 0) { #in-frame: assume no NMD
           print("new in frame SE")
         }
         else {
@@ -134,6 +130,7 @@ for (i in 1:length(test_seq)) {
 test_seq_trans <- DNAString(test_seq_trans)
 test_whole_trans_aa <- translate(test_seq_trans)
 countPattern("*", test_whole_trans_aa)
+#0. Good.
 
 #try to remove an exon:
 #for transcript ENST00000370141 it's exon 5
@@ -146,43 +143,12 @@ test_seq_se_trans <- DNAString(test_seq_se_trans)
 test_whole_se_trans_aa <- translate(test_seq_se_trans)
 #warning that last two bases were ignored. Out-of-frame! Hooray!
 countPattern("*", test_whole_se_trans_aa)
-#17!
+#17! Good!
 
 trans_gr <- ann_gtf[ann_gtf$transcript_id == "ENST00000370143"]
 #Error: logical subscript contains NAs
-#could possibly get around this error by subsetting based on genomic coordinates of gene
-
-#get (only) the CDS exons
-#if it matches the end of one, check the beginning of the next
-#if it matches, exit: it's an annotated splice junction that doesn't skip an exon in that isoform
-#if it doesn't match, store that exon and check the next one
-#continue till it matches
-#then take the skipped exon(s) and divide by 3
-#if divisible, it's an in-frame skip
 
 #to assess things at the end, will need to somehow compare depth (not just number I think)
 #of NMD-targeting splice junctions to other splice junctions
-#becuase eveerything is going to have some of both
-
-#at end of transcripts for that gene, put SJ into NMD list or not-NMD list
-
-
-#previous attempts with bed file:
-ann <- import("/Applications/Genomics_applications/Genomes_and_transcriptomes/hg38_plus_Akata_inverted.bed.converted.bed.reconfigured.bed")
-ovrlp <- findOverlaps(sj_gr, ann, type = "within", select = "first")
-#will need to deal with multiple isoforms
-sj_ann <- sj_gr
-sj_ann$name <- ann$name[ovrlp]
-#Error: subscript contains NAs when I try to bring over thick and blocks
-#not a problem with name (or score or itemRgb, though all have at least 1 NA)
-sj_ann$thick <- ann$thick[ovrlp]
-sj_ann$blocks <- ann$blocks[ovrlp]
-
-#check if SJ coordinates here are annotated (check donor & acceptor separately)
-#if so, check if there are annotated exons between
-#if so, look at exon(s) and divide by 3 to check for frameshift
-    #how to deal with multiple isoforms?
-#if no frameshift, assume no NMD
-#if frameshift, check for PTC
-#start with just junctions in annotated genes, with annotated donors & acceptors
+#because everything is going to have some of both
 
